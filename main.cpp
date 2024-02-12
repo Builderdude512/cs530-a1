6b90faf494f613ad678599b2e7e18733b6b95002
Connor Shands-Sparks <connorshandssparks@gmail.com>
2024-02-12 15:17:14 -0800

    onelineprint

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	int i = 0;
+	int offset = 0;
+	
+	std::ifstream file("data/files/main.cpp", std::ios::binary);
+	char buffer[16];
+
+	file.read(buffer, sizeof(buffer));
+	std:: streamsize bytesRead = file.gcount();
+	printf("%08x: ", offset);
+	for (i=0; i<16; i++) {
+		printf("%02x ",buffer[i]);
+	}
+	for (i=0; i<16; i++) {
+		printf("%c", buffer[i]);
+	}
+
+	// std::cout << "Bytes read: " << bytesRead << std::endl;
+
+	printf("\n");
 	return 0;
 }
 
