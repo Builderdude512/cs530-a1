a6763c6ff52056443f516ed95cca15a36c49faaa
Connor Shands-Sparks <connorshandssparks@gmail.com>
2024-02-14 16:17:28 -0800

    Draft v3

--- main.cpp
+++ main.cpp
@@ -17,16 +17,29 @@ bool printlinehex(std::ifstream &file) {
 	file.read(buffer, sizeof(buffer));
 	std:: streamsize bytesRead = file.gcount();
 	printf("%08x: ", offset);
-	for (i=0; i<16; i++) {
-		printf("%02x ",buffer[i]);
+	for (i=0; i<bytesRead; i++) {
+		printf("%02x",buffer[i]);
+		if(i%2 != 0){
+			printf(" ");
+		}
 	}
-	for (i=0; i<16; i++) {
+	for (; i<16; i++) {
+		printf("  ");
+		if(i%2 != 0){
+			printf(" ");
+		}
+	}
+	printf(" ");
+	for (i=0; i<bytesRead; i++) {
 		if(isprint(buffer[i])){
 			printf("%c", buffer[i]);
 		}
 		else{
 			printf(".");
 		}
+	}
+	for (; i < 16; i++) {
+		printf(" ");
 	}
 	offset+=bytesRead;
 	if(bytesRead<16) {
@@ -61,18 +74,27 @@ bool printlinebin(std::ifstream &file) {
 
 	file.read(buffer, sizeof(buffer));
 	std:: streamsize bytesRead = file.gcount();
+	if(bytesRead==0) {
+		return false;
+	}
 	printf("%08x: ", offset);
-	for (i=0; i<6; i++) {
+	for (i=0; i<bytesRead; i++) {
 		printbinary(buffer[i]);
 		printf(" ");
 	}
-	for (i=0; i<6; i++) {
+	for (; i<6; i++) {
+		printf("         ");
+	}
+	for (i=0; i<bytesRead; i++) {
 		if(isprint(buffer[i])){
 			printf("%c", buffer[i]);
 		}
 		else{
 			printf(".");
 		}
+	}
+	for (; i<6; i++) {
+		printf(" ");
 	}
 	offset+=bytesRead;
 	if(bytesRead<6) {
@@ -101,18 +123,20 @@ void printfilebin(std::ifstream &file) {
 	}
 }
 
-int main() {
-	
-	std::ifstream file("data/files/main.cpp", std::ios::binary);
+int main(int argc, char const * argv[]) {
 
-	int inp; 
-	printf("Type 1 if you want a hex translation, or 2 for binary \n");
-	std::cin >> inp; 
+	// if (argc < 2) {
+	// 	std::cout << "Enter file name" << std::endl;
+	// 	return 1;
+	// }
 
-	if(inp==1) {
+	if(/*argc==2*/ false) {
+		// std::ifstream file(argv[1], std::ios::binary);
+		std::ifstream file("data/files/main.cpp", std::ios::binary);
 		printfilehex(file);
 	} 
-	else if(inp==2) {
+	else if(/*argc==3*/ true) {
+		std::ifstream file("data/files/main.cpp", std::ios::binary);
 		printfilebin(file);
 	}
 	else {
